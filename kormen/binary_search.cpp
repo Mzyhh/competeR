9cec400fd8dd3758758334b21af1f8c1b500a264
Mzyhh <xiiad@mail.ru>
2025-07-04 19:48:02 +0300

    Add binary search algorithm.

---
+++ kormen/binary_search.cpp
@@ -0,0 +1,42 @@
+#include <iostream>
+#include <optional>
+
+
+/**
+ * Find index of value in array.
+ *
+ * Container should have sorted element between `left` and `right`.
+ *
+ * @param a Container in which we trying to find value. a[left:right] should be sorted.
+ * @param left Index of first element in sequence.
+ * @param right Length of sequence.
+ * @param value Target value.
+ * @return index of first entry of value in container `a` or std::nullopt if there is no such value.
+ */
+template <typename V>
+std::optional<int> binary_search(const V* const a, const int left, const int right, const V value) {
+    if (left > right) return std::nullopt;
+    int mid = (left + right) / 2;
+    if (a[mid] == value) return mid;
+    if (value < a[mid]) return binary_search(a, left, mid - 1, value);
+    return binary_search(a, mid + 1, right, value);
+}
+
+
+int main(int argc, char *argv[]) {
+    if (argc < 3) {
+        std::cout << "Using: " << argv[0] << " a[0] a[1] ... a[n-1] value\n";
+        return -1;
+    } 
+    int n = argc - 2;
+    int *arr = new int[n];
+    for (int i = 1; i < argc - 1; ++i)
+        arr[i] = atoi(argv[i]);
+    int value = atoi(argv[argc - 1]);
+    auto index = binary_search(arr, 0, n, value);
+    if (index.has_value())
+        std::cout << "Value: " << value << " is firstly met on " << index.value()  - 1<< "th position\n";
+    else
+        std::cout << "Value: " << value << " not found.\n";
+    return 0;
+}
